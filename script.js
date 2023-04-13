60e6c65bf314fad6290eb33e2b0f78cd33c0aacb
Sofia Lessona <sofi.lessona@gmail.com>
2023-04-13 02:07:38 -0300

    avanzando  seccion video

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ let video=document.getElementById('video');
 
 const play=()=>{
    video.play();
+   document.querySelector('#duracion').textContent=video.duration.toFixed(2);
 }
+
 const pause=()=>{
     video.pause();
- }
+}
